e69abd682c2915698349d66d444fa0ef8e00ccb7
Polalf <103451330+Polalf@users.noreply.github.com>
2022-09-05 18:57:06 -0300

    aaaaaaaa
    a

--- taller 4/Assets/Scripts/player.cs
+++ taller 4/Assets/Scripts/player.cs
@@ -1,19 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public int bullets = 10;
-
+    [SerializeField] private Animator playerAnimator;
+    public float rotate;
+    public float speed;
+    private Rigidbody2D Rb;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform shootctrl;
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        //movimiento
+        float moveY = Input.GetAxisRaw("Vertical");
+        if (moveY == 1)
+        {
+            Rb.velocity = transform.up * -speed;
+        }
+        if(moveY == -1)
+        {
+            Rb.velocity = transform.up * speed;
+        }
+        float moveX = Input.GetAxisRaw("Horizontal");
         
+
+        if (moveX == -1)
+        {
+            transform.Rotate(0, 0, rotate * Time.deltaTime);
+        }
+        if (moveX == 1)
+        {
+            transform.Rotate(0, 0, -rotate * Time.deltaTime);
+        }
+        playerAnimator.SetFloat("speed", moveY);
+
+        // disparo
+        if(Input.GetButtonDown("Fire1"))
+        {
+
+        }
+     
     }
+    private void Shoot()
+    {
+        //Instantiate(bullet, shootctrl )
+    }
+                
+    
+
 }
